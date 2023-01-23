e167b52285d41fde2f0cb8df5e79c4cb29c5a7fd
JenniferCorderoR <jennifer.factoriaf5@gmail.com~>
2023-01-23 10:00:51 +0100

    added solutions

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,5 +1,13 @@
 function fizzbuzz(num) {
-
+    if (((num % 5) == 0) && !((num % 3) == 0)) {
+     return 'Buzz'
 }
-
+    if (((num % 3) == 0) && !((num % 5) == 0)){
+    return 'Fizz'
+}
+if (((num % 3) == 0) && ((num % 5) == 0)){
+    return 'FizzBuzz'
+}
+}
 export { fizzbuzz }
+
